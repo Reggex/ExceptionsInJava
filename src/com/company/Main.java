e303919ef8181b5d01e0883208235ced9eb780d3
Reggex <sedyhnastenka@mail.ru>
2023-03-28 16:10:09 +0300

    Create project

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for(int i = 65; i <= 122; i++)
+        {
+            System.out.println(" The ASCII value of " + (char)i + "  =  " + i);
+        }
+
+        char a = 'a';
+        System.out.println('a' > 'b' | '_' == 'b' | '0' > 'b');
+        //ограничить с 0 по 9
+        //ограничить с a по z
+        //ограничить с A по Z
+        // "или равно _"
+
+        //проверка количества символов вначале.
+        //если это пароль, то проверка на соответсвие.
+        //проверка на символы.
+
+    }
+}
